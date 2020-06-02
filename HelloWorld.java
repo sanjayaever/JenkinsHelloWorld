8c48c1f48ec493e8c1c9ddeacdc7e7f1d29af045
sanjayaever <sanjayaever@gmail.com>
2020-06-02 12:28:01 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //A Very Simple Example
 class HelloWorld {
   public static void main(String[] args){
-    System.out.println("I'm a Simple Program");
+    System.out.println("I'm a Simple Program..made some changes");
   }
 }

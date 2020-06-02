65c3cd7cf45359bd1e075d387fad646885dd7c1d
sanjayaever <sanjayaever@gmail.com>
2020-06-02 14:49:43 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //A Very Simple Example
 class HelloWorld {
   public static void main(String[] args){
-    System.out.println("I'm a Simple Program..made some changes");
+    System.out.println("I'm a Simple Program..trying webhook");
   }
 }
